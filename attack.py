218942eec1b90db2b363210602b4376f70c2b90f
root <root@localhost.localdomain>
2019-02-07 11:26:35 -0700

    attack.py argument processing

--- attack.py
+++ attack.py
@@ -1,11 +1,35 @@
 #! /usr/bin/env python3
 
-
+import sys, re
 import time, os
+
+from socket import *
 pid = os.getpid()
 
-print("dummy attack program with pid %d.  do not trust my results" % pid)
+listen_addr = ("", 50000)
+send_addr = ("", 50010)
 
-time.sleep(1)                        # sleep for 1 second
 
-print("%d: attacking at %s" % (pid, time.asctime(time.localtime()))) # print time
+def usage(): 
+    print("""Usage: %s \n
+            Options                        Default     Description
+            [-l host:port]                   50000     Listen port number
+            [-s host:port]                   50010     Send message to port number
+            """ % sys.argv[1])
+    sys.exit(1)
+
+try: 
+    args = sys.argv[1:]
+    while args: 
+        sw = args[0]; del args[0]
+        if sw == "-l": 
+            addr, port = re.split(":",args[0]); del args[0]
+            listen_addr = (addr, port)
+        elif sw == "-s": 
+            addr, port = re.split(":", args[0]); del args[0]
+            send_addr = (addr, port)
+        else: 
+            usage()
+#time.sleep(1)                        # sleep for 1 second
+
+
