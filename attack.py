6b5e82c661a650acaddffbed94c568f36ee1d9a3
Ederrod <ederrod@UTEP-3MSX082.utep.edu>
2019-02-11 11:43:03 -0700

    setting up listening and sending sockets

--- attack.py
+++ attack.py
@@ -11,13 +11,10 @@ send_addr = ("", 50010)
 
 
 def usage(): 
-    print("""Usage: %s \n
-            Options                        Default     Description
-            [-l host:port]                   50000     Listen port number
-            [-s host:port]                   50010     Send message to port number
-            """ % sys.argv[1])
+    print("Usage: %s -l <host:port> -s <host:port>" % sys.argv[1])
     sys.exit(1)
 
+# argument processing
 try: 
     args = sys.argv[1:]
     while args: 
@@ -29,7 +26,15 @@ try:
             addr, port = re.split(":", args[0]); del args[0]
             send_addr = (addr, port)
         else: 
+            print("Unexpected argument!")
             usage()
-#time.sleep(1)                        # sleep for 1 second
 
+#time.sleep(1)                        # sleep for 1 second
+send_socket = scoket(AF_INET, SOCK_DGRAM) # send messages socket (Client socket)
+
+listen_socket = socket(AF_INET, SOCK_DGRAM) # listening for incoming messages socket (Server socket)
+listen_socket.bind(listen_addr) #bind
+""" CSMA/CD implementation: 
+     
+"""
 
